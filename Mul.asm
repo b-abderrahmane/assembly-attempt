aa62a14513dcff063c8f4ed1b3bc4c6e13d4c151
abdou <b-abderrahmane@outlook.com>
2018-05-13 12:29:59 +0200

    added renamed files

---
+++ Mul.asm
@@ -0,0 +1,20 @@
+global _start
+
+
+section .text
+
+_start:
+
+	mov eax, 0xffffffff
+	mov bx, 0x6
+	mul bx
+	
+	mov eax, 0x1
+	mov ebx, 0x0
+	int 0x80
+
+section .data
+
+	message: db "Hello world!", 0xA, 0xD
+	mlen equ $-message
+	var: db 0x69
